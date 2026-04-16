d400eddb5299d2d7d4b7205bb7b432e0617106cb
sayyidquthbi <sayyidquthbi39@gmail.com>
2026-04-16 21:58:34 +0700

    first upload

---
+++ gg.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("teman ku semua")
+print("pada jahat tante")
+print("nama aku sayyid")
---
+++ hh.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+USERNAME = "sayang"
+PASSWORD = "123"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["username"] == USERNAME and request.form["password"] == PASSWORD:
+            return redirect("/home")
+        else:
+            return "Login gagal!"
+
+    return render_template("login.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+app.run(debug=True)
